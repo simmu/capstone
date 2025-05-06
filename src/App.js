4d4ca20b1301870fde8e798de5d19124d18d5ce5
simmu <simmuji@gmail.com>
2025-05-06 12:07:46 -0400

    Refactor App.js layout and remove unused reportWebVitals import from index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <header className="App-header"></header>
+      <div className='hero'></div>
+      <section id='new-menu'></section>
+      <section id='booking'></section>
+      <section id='business-hours'></section>
+      <section id='customer-reviews'></section>
+      <footer></footer>
     </div>
   );
 }
